9f1cfca04a407d05191fe22bdc04a3acd88a81af
Fabiano Bandeira <lfabianosb@gmail.com>
2017-07-12 21:06:06 -0300

    Representa o voo mais barato

---
+++ src/main/java/model/Flight.java
@@ -0,0 +1,23 @@
+package model;
+
+public class Flight {
+	private String cia;
+	private String price;
+
+	public String getCia() {
+		return cia;
+	}
+
+	public void setCia(String cia) {
+		this.cia = cia;
+	}
+
+	public String getPrice() {
+		return price;
+	}
+
+	public void setPrice(String price) {
+		this.price = price;
+	}
+
+}
